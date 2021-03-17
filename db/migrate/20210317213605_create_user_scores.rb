4d1b130c9784717e922331d9bb061f53f905fc21
Werter Meira <wertermeira@gmail.com>
2021-03-17 18:39:27 -0300

    model

---
+++ db/migrate/20210317213605_create_user_scores.rb
@@ -0,0 +1,10 @@
+class CreateUserScores < ActiveRecord::Migration[6.1]
+  def change
+    create_table :user_scores do |t|
+      t.references :user, null: false, foreign_key: true
+      t.integer :max_score
+
+      t.timestamps
+    end
+  end
+end
