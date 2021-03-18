5d2d2cb2d07a9724771dcb8f0cd7ed512f447872
Werter Meira <wertermeira@gmail.com>
2021-03-18 15:16:20 -0300

    remove and fix

--- app/serializers/user_score_serializer.rb
+++ app/serializers/user_score_serializer.rb
@@ -1,4 +1,8 @@
 class UserScoreSerializer < ActiveModel::Serializer
-  attributes :id, :max_score
+  attributes :id, :max_score, :position
   has_one :user, serializer: UserSerializer
+
+  def position
+    scope[:ids].index(object.id) if scope[:ids].present?
+  end
 end
